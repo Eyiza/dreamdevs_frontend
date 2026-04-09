7c336751220aee656797b3cbe19a2578dbffc3b0
Precious Michael <precious.michael2002@gmail.com>
2026-04-09 15:51:21 +0100

    add: educlass ui

--- js/async.js
+++ js/async.js
@@ -12,15 +12,26 @@ console.log("I am the Fourth");
 
 const PRODUCT_URL = "https://dummyjson.com/products";
 
-function getProducts(url) {
-    fetch(url)
-        .then((response) => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error("Error fetching products:", error);
-        });
+// function getProducts(url) {
+//     fetch(url)
+//         .then((response) => response.json())
+//         .then(data => {
+//             console.log(data);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
+
+async function getProducts(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
 }
+
 
 getProducts(PRODUCT_URL);
