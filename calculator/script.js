83413950d973d7d232e5723273e21c03f9a55e8c
Precious Michael <precious.michael2002@gmail.com>
2026-04-11 11:38:49 +0100

    add: input digit in calculator logic

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+let current = '0';
+const currentElement = document.getElementById('current');
+
+function updateDisplay() {
+    currentElement.textContent = current;
+}
+
+const inputNumber = document.querySelector(".cal-buttons")
+inputNumber.addEventListener("click", (event) => {
+    // console.log(event)
+    if (event.target.classList.contains("button") 
+        && !event.target.classList.contains("clear") 
+        && !event.target.classList.contains("operator") 
+        && !event.target.classList.contains("equals")) {
+        inputDigit(event.target.textContent);
+    }
+})
+
+function inputDigit(digit) {
+    if (current === '0') {
+        current = digit;
+    } else {
+        current += digit;
+    }
+    updateDisplay();
+}
