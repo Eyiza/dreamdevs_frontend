525c51ce2d747e1e105e0f4db05e746447ef410a
Precious Michael <precious.michael2002@gmail.com>
2026-04-16 12:43:08 +0100

    add: counter app

---
+++ redux/src/component/Counter.jsx
@@ -0,0 +1,18 @@
+import { increment, decrement } from '../slice/counterSlice'
+import { useSelector, useDispatch } from 'react-redux'
+
+const Counter = () => {
+  const value = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+        <h1>Counter</h1>
+        <p>count: {value}</p>
+        <button onClick={() => dispatch(increment())}>Increment+</button>
+        <button onClick={() => dispatch(decrement())}>Decrement-</button>
+    </div>
+  )
+}
+
+export default Counter
