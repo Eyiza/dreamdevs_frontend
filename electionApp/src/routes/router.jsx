8c4c958287a9c8990167507a00480d32b1ab9eee
Precious Michael <precious.michael2002@gmail.com>
2026-04-21 10:48:10 +0100

    election app ui setup

---
+++ electionApp/src/routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        // element: <Login />
+    },
+])
+
+export default router;
